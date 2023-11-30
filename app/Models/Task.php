ae53ecf7563569b49aab8919d4573d171f6eec78
Arnold Wafula <arnold.wafula0@gmail.com>
2023-11-30 12:21:27 +0300

    Created Task model

---
+++ app/Models/Task.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['task_name','description','due_date','assigned_to','completed'];  
+
+    public function assignedTo() {
+        return $this->belongsTo(User::class, 'assigned_to');
+    }
+}
