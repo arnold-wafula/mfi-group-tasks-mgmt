9c0bba20081dd62b6e562cc14e717f741ad36e13
Arnold Wafula <arnold.wafula0@gmail.com>
2023-12-01 22:19:56 +0300

    Form security added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,46 +5,23 @@
     <title>Log In</title>
 </head>
 <body>
-    <form method="POST" action="">
-        <div>
-            <label for="name">Name</label>
-            <input type="text" name="name" placeholder="Name"/>
-        </div>
-        <div>
-            <label for="email">Email</label>
-            <input type="email" name="email" placeholder="Email"/>
-        </div>
+<form method="POST" action="{{ route('register.store') }}">
+        @csrf
         <div>
             <label for="eid">Employee ID</label>
             <input type="text" name="eid" placeholder="23476271"/>
-        </div>
-        <div>
-            <label for="dept">Department</label>
-            <select name="department">
-                <option value="sales">Sales</option>
-                <option value="it">I.T</option>
-                <option value="marketing">Marketing</option>
-                <option value="hr">Human Resources</option>
-            </select>
-        </div>
-        <div>
-            <label for="name">Designation</label>
-            <select name="designation" id="">
-                <option value="junior">Junior</option>
-                <option value="mid">Mid-level</option>
-                <option value="senior">Senior</option>
-            </select>
+            @if($errors->has('eid'))
+            <span>{{ $errors->first('eid') }}</span>
+            @endif
         </div>
         <div>
             <label for="password">Password</label>
             <input type="password" name="password" placeholder="Enter password">
+            @if($errors->has('password'))
+            <span>{{ $errors->first('password') }}</span>
+            @endif
         </div>
-        <div>
-            <label for="repeat_password">Repeat Password</label>
-            <input type="password" name="repeat_password" placeholder="Repeat Password">
-        </div>
-
-        <button type="submit">Log in</button>
+        <button type="submit">Register</button>
         <button type="reset">Cancel</button>
     </form>
 </body>
