2e9a796f1c3489e3a8afa5a7065ab7bc34af2dae
Arnold Wafula <arnold.wafula0@gmail.com>
2023-12-05 18:03:20 +0300

    Task handling controller functions added

--- app/Http/Controllers/Task/TaskController.php
+++ app/Http/Controllers/Task/TaskController.php
@@ -4,36 +4,85 @@ namespace App\Http\Controllers\Task;
 
 use App\Http\Controllers\Controller;
 use App\Models\Task;
-use Illuminate\Support\Facades\Request;
+use App\Models\User;
+use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    // Create a task
-    public function create(Request $request) {
-        // Validate the form data
+
+    public function index() {
+        //$tasks = Task::all();
+        $tasks = Task::with(['assignedTo', 'createdBy'])->get();
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function create() {
+        //$users = User::all(); // Fetch all users for the assignment dropdown
+        $users = User::where('id', '!=', auth()->id())->get();
+        return view('tasks.create', compact('users'));
+
+        //return view('tasks.create');
+    }
+
+    public function store(Request $request) {
+        // Validate the input data
         $request->validate([
             'task_name' => 'required|string|max:255',
             'description' => 'required|string',
+            'priority' => 'required|in:high,medium,low',
             'due_date' => 'required|date',
-            'assigned_to' => 'required|string',
+            'assigned_to' => 'required|array',
+            'assigned_to.*' => 'exists:users,id',
             'completed' => 'required|string'
         ]);
 
-        // Create a new task
-        Task::create($request->all());
+        // Create the task
+        //Task::create($request->all());
 
-        return redirect()->route('task.show')->with('success', 'Task created successfully');
+        //$task = Task::create($request->except('assigned_to'));
+
+        $task = Task::create([
+            'task_name' => $request->input('task_name'),
+            'description' => $request->input('description'),
+            'priority' => $request->input('priority'),
+            'due_date' => $request->input('due_date'),
+            'completed' => $request->input('completed'),
+            'created_by' => auth()->id()
+        ]);
+
+        // Attach users to the task
+        $task->users()->attach($request->input('assigned_to'));
+
+        //return redirect()->route('tasks.index')->with('success', 'Task created successfully.');
+        
+        return redirect()->route('dashboard')->with('success', 'Task created successfully.');
     }
 
-    // Display created tasks
-    public function show() {
-        // Retrieve all tasks from the database
-        $tasks = Task::all();
-
-        return view('task.show', ['tasks' => $tasks]);
+    public function edit(Task $task) {
+        return view('tasks.edit', compact($task));
     }
 
-    public function delete() {
-        return view('task.delete');
+    public function update(Request $request, Task $task) {
+        // Validate the input data
+        $request->validate([
+            'task_name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'priority' => 'required|in:high,medium,low',
+            'due_date' => 'required|date',
+            'assigned_to' => 'required|array',
+            'assigned_to.*' => 'exists:users,id',
+            'completed' => 'required|string'
+        ]);
+
+        // Update the task
+        $task->update($request->all());
+
+        return redirect()->route('tasks.index')->with('success', 'Task updated successfully');
+    }
+
+    public function destroy(Task $task) {
+        // Delete the task
+
+        return redirect()->route('tasks.index')->with('success', 'Task deleted successfully');
     }
 }
