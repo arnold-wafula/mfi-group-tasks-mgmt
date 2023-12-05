f45e422da0c791f104d47c7e3386478d2733c626
Arnold Wafula <arnold.wafula0@gmail.com>
2023-12-05 18:10:21 +0300

    Reusable app.blade.php file created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,19 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MFI TASKS MANAGEMENT SYSTTEM</title>
+
+     <!-- Bootstrap CSS -->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
-    <header>
-        <h1>Your App Header</h1>
-    </header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="{{ route('dashboard') }}">MFI Task Management System</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('dashboard') }}">Close</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
-    <main>
+    <main class="container mt-4">
         @yield('content') 
         <!-- This is where the specific view content will be injected -->
     </main>
 
-    <footer>
-        <p>&copy; 2023 Your App</p>
-    </footer>
+    <!-- Bootstrap JS (optional, if you need Bootstrap JavaScript features) -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
 </html>
