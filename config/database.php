6e444b1feca27ad37e26643d1101087e640ad571
Arnold Wafula <arnold.wafula0@gmail.com>
2023-12-06 09:27:20 +0300

    mysql changes

--- config/database.php
+++ config/database.php
@@ -45,18 +45,18 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('STACKHERO_MYSQL_HOST'),
-            'port' => 3306,
-            'username' => env('STACKHERO_MYSQL_USER'),
-            'password' => env('STACKHERO_MYSQL_PASSWORD'),
-            'database' => env('STACKHERO_MYSQL_USER'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'sslmode' => 'require',
+            'sslmode' => 'prefer',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 ]) :
